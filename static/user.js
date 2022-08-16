f340b972c64ba40ec8ee6567503162d95c8cbd8a
yashwant <yashwantyadav71@gmail.com>
2022-08-16 10:01:01 +0530

    first commit

---
+++ static/user.js
@@ -0,0 +1,22 @@
+const socket = io('https://temprature-ui-app.herokuapp.com/');
+console.log("hello running")
+// const socket = io('http://localhost:4000');
+
+var txtInp =document.getElementById('inputBox');
+var formContainer =document.getElementById('formContainer');
+var output =document.getElementById('output')
+
+socket.on('append-text',str=>{ 
+    console.log(str);
+    var div =document.createElement('div');
+    div.innerText=str;
+    output.append(div)
+    // output.innerText=str;
+})
+// formContainer.addEventListener('submit' ,(e)=>{
+//     // e.preventDefault(); 
+//     // txtInp.value='';
+//     setTimeout(() => {
+//         txtInp.value='';
+//     },100);
+// })
