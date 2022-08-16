286c2c8d01741ddde1cbd24b9e5eb2fc373fb4df
yashwant <yashwantyadav71@gmail.com>
2022-08-16 12:06:36 +0530

    make it better

--- index.js
+++ index.js
@@ -13,21 +13,18 @@ app.use('/static', express.static('static')) // For serving static files
 app.use(express.urlencoded())
 
 //to server html file
-app.get('/rend', (req, res) => {
+app.get('/', (req, res) => {
     res.render(__dirname + '/index.html', params)
 })
 
-// io.on('connection', socket => {
-    app.post('/', async (req, res) => {
-        var str = req.body.text;
+io.on('connection',socket=>{
+    socket.on('postCities',str=>{
+        // console.log(str);
         var arr = str.split(',')
         var obj = {
             "cities": arr
         }
-        res.render(__dirname + '/index.html', obj)
-        console.log("hello")
         console.log(obj)
-
         axios.post('https://weather-temp-api.herokuapp.com/getWeather', obj)
             .then((res) => {
                 // console.log(res.data);
@@ -41,6 +38,32 @@ app.get('/rend', (req, res) => {
                 console.log("issue with api");
             });
     })
+})
+// io.on('connection', socket => {
+
+    // app.post('/posting', async (req, res) => {
+    //     var str = req.body.text;
+    //     var arr = str.split(',')
+    //     var obj = {
+    //         "cities": arr
+    //     }
+    //     res.render(__dirname + '/index.html', params)
+    //     console.log("hello")
+    //     console.log(obj)
+
+    //     axios.post('https://weather-temp-api.herokuapp.com/getWeather', obj)
+    //         .then((res) => {
+    //             // console.log(res.data);
+    //             var obj2 = res.data;
+    //             var mainobj = obj2["weather"]
+    //             // res.send(mainobj);
+    //             var strmainobj = JSON.stringify(mainobj)
+    //             console.log(mainobj)
+    //             io.emit('append-text', strmainobj)
+    //         }).catch((err) => {
+    //             console.log("issue with api");
+    //         });
+    // })
 
 // })
 
--- static/user.js
+++ static/user.js
@@ -1,22 +1,21 @@
-// const socket = io('https://temprature-ui.herokuapp.com') ;
+const socket = io('https://temprature-ui.herokuapp.com') ;
 console.log("hello running")
-const socket = io('http://localhost:4000');
+// const socket = io('http://localhost:4000');
 
 var txtInp =document.getElementById('inputBox');
 var formContainer =document.getElementById('formContainer');
 var output =document.getElementById('output')
-
+var button =document.getElementById('submit')
 socket.on('append-text',str=>{ 
+    // console.log('append working')
     console.log(str);
     var div =document.createElement('div');
     div.innerText=str;
     output.append(div)
     // output.innerText=str;
 })
-// formContainer.addEventListener('submit' ,(e)=>{
-//     // e.preventDefault(); 
-//     // txtInp.value='';
-//     setTimeout(() => {
-//         txtInp.value='';
-//     },100);
-// })
+button.addEventListener('click' ,(e)=>{
+    var str=txtInp.value;
+    console.log(str);
+    socket.emit("postCities",str)
+})
